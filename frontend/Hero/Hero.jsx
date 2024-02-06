d3fe28fbb5667bb6fca31555e7a329e93ecb0d21
Rakshitha <112706233+Rakshithaashok@users.noreply.github.com>
2024-02-06 20:21:28 +0530

    Create Hero.jsx

---
+++ frontend/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Hero.css'
+import hand_icon from '../Assets/hand_icon.png'
+import arrow_icon from '../Assets/arrow.png'
+import hero_image from '../Assets/hero_image.png'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className='hero-left'>
+        <h2>Best Deals Across Platform</h2>
+        <div>
+            <div className='hero-hand-icon'>
+                <p>Hot</p>
+                <img src={hand_icon} alt=""/>
+            </div>
+            <p>Picks from</p>
+            <p>Top Retailers</p>
+        </div>
+        <div className='hero-latest-btn'>
+            <div>Compare Prices Instantly</div>
+            <img src={arrow_icon} alt=""/>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Hero
